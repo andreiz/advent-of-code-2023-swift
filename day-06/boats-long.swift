88a2551fa2521afce8ef98510646e972be3eafd4
Andrei Zmievski <andrei@zmievski.org>
2024-01-02 13:52:49 -0800

    Update parsing to squish numbers together, refs #6

--- day-06/boats-long.swift
+++ day-06/boats-long.swift
@@ -1,23 +1,19 @@
 import Foundation
 
-func parseInputFile(filename: String) -> [(Int, Int)]? {
+func parseInputFile(filename: String) -> (Int, Int)? {
     do {
         let content = try String(contentsOfFile: filename, encoding: .utf8)
         let lines = content.split(separator: "\n")
         guard lines.count == 2 else {
             print("Invalid input format")
-            return []
+            return nil
         }
 
-        let timeNumbers = lines[0].split(separator: " ").compactMap { Int($0) }
-        let distanceNumbers = lines[1].split(separator: " ").compactMap { Int($0) }
+        // split on whitespace, drop first element, join the rest back together and convert to Int
+        let time = Int(lines[0].split(separator: " ").dropFirst().joined())!
+        let distance = Int(lines[1].split(separator: " ").dropFirst().joined())!
 
-        guard timeNumbers.count == distanceNumbers.count else {
-            print("Mismatched number of times and distances")
-            return []
-        }
-
-        return Array(zip(timeNumbers, distanceNumbers))
+        return (time, distance)
     } catch {
         print("Error reading file \(filename)")
         return nil
@@ -48,16 +44,14 @@ guard CommandLine.arguments.count == 2 else {
 }
 
 let filename = CommandLine.arguments[1]
-if let result = parseInputFile(filename: filename) {
+if let (time, distance) = parseInputFile(filename: filename) {
     var ways = [Int]()
 
-    for (time, distance) in result {
-        let (solution1, solution2) = solveQuadratic(time: time, distance: distance)
-        if let x1 = solution1, let x2 = solution2, x1 > 0, x2 > 0 {
-            let x1Int = x1 == floor(x1) ? Int(x1) + 1 : Int(x1.rounded(.up))
-            let x2Int = x2 == floor(x2) ? Int(x2) - 1 : Int(x2.rounded(.down))
-            ways.append(x2Int - x1Int + 1)
-        }
+    let (solution1, solution2) = solveQuadratic(time: time, distance: distance)
+    if let x1 = solution1, let x2 = solution2, x1 > 0, x2 > 0 {
+        let x1Int = x1 == floor(x1) ? Int(x1) + 1 : Int(x1.rounded(.up))
+        let x2Int = x2 == floor(x2) ? Int(x2) - 1 : Int(x2.rounded(.down))
+        ways.append(x2Int - x1Int + 1)
     }
     print(ways.reduce(1, *))
 }
